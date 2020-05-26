133ccb33bb90fec26a7e46f3b89287fd6dfe2fe6
Kenji Ogura <k5i.ogura.v40@gmail.com>
2020-05-26 16:55:20 +0900

    revise

--- map-voc.sh
+++ map-voc.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-options="i:r:d:"
+options="i:g:r:d:"
 usage () {
     echo Usage: $0 $options
     echo \-i : image paths list for Dataset
+    echo \-g : ground truth paths list for Dataset
     echo \-r : result files in the directory
     echo \-d : DB directory for GroundTruth
     exit
 }
+image_list=""
+dbname=""
+result=""
+gt_list=""
 while getopts $options OPT;do
     case $OPT in
     i)
-        if [ ! -f $OPTARG ];then
+        image_list=$OPTARG
+        if [ ! -f $image_list ];then
             echo "\-i image_path_list";usage
         fi
-        dbname=$(basename $OPTARG)
+        dbname=$(basename $image_list)
         dbname=${dbname%.txt}
-        if [ -f $OPTARG ];then      # OPTARG Images list file
-            if [ ! -d $dbname ] || [ $OPTARG -nt $dbname ]; then # -nt newer_than
-                ./convert_gt_yolo.sh $OPTARG
-            else
-                echo --- DB Directory newer than $OPTARG
-            fi
+        ;;
+    g)
+        gt_list=$OPTARG
+        if [ ! -f $gt_list ];then
+            echo "\-i groundtruth_path_list";usage
         fi
         ;;
     d)
@@ -31,18 +36,27 @@ while getopts $options OPT;do
         fi
         ;;
     r)
-        if [ ! -d $OPTARG ];then    # OPTARG Result directory of inference
-            echo not found $OPTARG file;usage
-        fi
         result=$OPTARG
+        if [ ! -d $result ];then    # OPTARG Result directory of inference
+            echo not found $result file;usage
+        fi
         ;;
     ?)  echo unknown args;usage
         ;;
     esac
 done
-if [ -z ${dbname+x} ] || [ -z ${result+x} ];then
+if [ -z "${dbname}" ] || [ -z "${result}" ];then
+    echo need $dbname $result
     usage
 fi
+
+if [ -f $image_list ];then      # OPTARG Images list file
+    if [ ! -d $dbname ] || ([ ! -z "$image_list" ] && [ $image_list -nt $dbname ]) || ([ ! -z "$gt_list" ] && [ $gt_list -nt $dbname ]) ; then # -nt newer_than
+        echo --- Creating DB ${dbname} ..
+        ./convert_gt_yolo.sh $image_list $gt_list
+    fi
+fi
+
 echo --- DB=$dbname RESULT=$result
 
 echo --- Import $result into $dbname
